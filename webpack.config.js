3bcb3b95c3d37063fb349b73a8a086e9c4f7678b
Akanksha Singh <akanksha.singh@nineleaps.com>
2021-10-02 17:28:01 +0530

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,50 @@
-//entry -> output
-
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map', //To configure source maps (know in which file error occured instead of in bundle.js file)
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true            //Router config for client side routing
-    }
-};
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map', //To configure source maps (know in which file error occured instead of in bundle.js file)
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true            //Router config for client side routing
+        }
+    };
+}
+
